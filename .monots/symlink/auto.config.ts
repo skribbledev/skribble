b97cc43551b0a75662cd2e48a7e07b8f22e3c290
Ifiok Jr <ifiokotung@gmail.com>
2022-02-14 13:09:19 +0000

    ci: basic auto configuration

---
+++ .monots/symlink/auto.config.ts
@@ -0,0 +1,30 @@
+import { AutoRc } from 'auto';
+
+import { INpmConfig } from '@auto-it/npm';
+import { IAllContributorsPluginOptions } from '@auto-it/all-contributors';
+
+const npmOptions: INpmConfig = {
+  exact: true,
+  canaryScope: '@auto-canary',
+};
+
+const allContributorsOptions: IAllContributorsPluginOptions = {
+  types: {
+    plugin: '**/plugin/**/*',
+    code: ['**/src/**/*', '**/package.json', '**/tsconfig.json'],
+    doc: ['**/*.md', '**/*.mdx'],
+    test: ['**/*.spec.{ts,tsx}', 'crates/**/tests/*'],
+  },
+};
+
+/** Auto configuration */
+export default function rc(): AutoRc {
+  return {
+    plugins: [
+      'released',
+      ['npm', npmOptions],
+      ['crates', {}],
+      ['all-contributors', allContributorsOptions],
+    ],
+  };
+}
